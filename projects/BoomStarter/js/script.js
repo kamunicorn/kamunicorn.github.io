087f23776712486a1cd54adf7da18fd2c24700aa
Vera Perelygina <38280676+kamunicorn@users.noreply.github.com>
2020-09-25 22:44:20 +1200

    бумстартер - третий подход правок

--- projects/BoomStarter/js/script.js
+++ projects/BoomStarter/js/script.js
@@ -6,10 +6,11 @@ function initSwitchSoundButton() {
     var $switchers = $('.js-switch-sound');
 
     $switchers.click(function(e) {
-        let $switch = $(e.target),
-            $video = $('#' + $switch.data('video'))[0];
+        let $target = $(e.target),
+            $switcher = ($target.hasClass('icon')) ? $target.parent() : $target,
+            videoElem = $('#' + $switcher.data('video'))[0];
         
-        $video.muted = !$video.muted;
-        $switch.toggleClass('is-sound-off is-sound-on');
+        videoElem.muted = !videoElem.muted;
+        $switcher.toggleClass('is-sound-off is-sound-on');
     });
 }
