ce1514c74247a57ed0041d6f83fbc500765400fc
Vera Perelygina <38280676+kamunicorn@users.noreply.github.com>
2020-09-26 23:26:26 +1200

    js-закладки, звук в видео, bugfix обрезка слайдера

--- projects/BoomStarter/js/script.js
+++ projects/BoomStarter/js/script.js
@@ -1,16 +1,55 @@
 $(function() {
-    initSwitchSoundButton();
+    initSoundSwitchers();
+    initBookmarks();
 });
 
-function initSwitchSoundButton() {
+function initBookmarks() {
+    var $bookmarks = $('.js-add-bookmark');
+    
+    $bookmarks.each( (i, elem) => {
+        let $bookmark = $(elem);
+
+        $bookmark.click(function() {
+            let projectId = $bookmark.data('card'),
+                action = ($bookmark.hasClass('is-filled')) ? 'delete' : 'add',
+                // action = ($bookmark.hasClass('is-filled')) ? 'delete-bookmark' : 'add-bookmark',
+                request = {
+                    'action': action,
+                    'projectId': projectId
+                };
+            console.log($bookmark);
+            console.log(request);
+
+            $.ajax('api/add-bookmark.json', {
+                method: "POST",
+                data: request,
+                success: function(response) {
+                    console.log(response);
+                    let processedProjectId = response.projectId,
+                        // processedProjectId = projectId,
+                        $btn = $(`.js-add-bookmark[data-project="${processedProjectId}"]`);
+                    
+                    console.log($btn);
+                    // $btn.toggleClass('is-filled');
+                    $bookmark.toggleClass('is-filled');
+
+                }
+            });
+
+        });
+    });
+}
+
+function initSoundSwitchers() {
     var $switchers = $('.js-switch-sound');
 
-    $switchers.click(function(e) {
-        let $target = $(e.target),
-            $switcher = ($target.hasClass('icon')) ? $target.parent() : $target,
-            videoElem = $('#' + $switcher.data('video'))[0];
+    $switchers.each( (i, elem) => {
+        let $switcher = $(elem);
         
-        videoElem.muted = !videoElem.muted;
-        $switcher.toggleClass('is-sound-off is-sound-on');
+        $switcher.click(function() {
+            let videoElem = $('#' + $switcher.data('video'))[0];
+            videoElem.muted = !videoElem.muted;
+            $switcher.toggleClass('is-sound-off is-sound-on');
+        });
     });
 }
